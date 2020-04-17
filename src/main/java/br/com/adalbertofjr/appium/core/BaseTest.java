e86c078ae46bd3264f83f04ceb8bf0b3900726a2
Adalberto Fernandes Jr <adalberto.fernandesjr@gmail.com>
2020-04-17 17:54:07 -0300

    Reset app

--- src/main/java/br/com/adalbertofjr/appium/core/BaseTest.java
+++ src/main/java/br/com/adalbertofjr/appium/core/BaseTest.java
@@ -1,11 +1,18 @@
 package br.com.adalbertofjr.appium.core;
 
 import org.junit.After;
+import org.junit.AfterClass;
 
 public class BaseTest {
 
+	@AfterClass
+	public static void finalizarClasse() {
+		DriverFactory.killDriver();
+		
+	}
+
 	@After
 	public void tearDown() {
-		DriverFactory.killDriver();
+		DriverFactory.getDriver().resetApp();
 	}
 }
